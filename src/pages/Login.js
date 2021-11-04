9083e6816595733bfe1ceda8c4a1d7f5b852e3c4
Rafael <rafael.abreu@allycode.com.br>
2021-11-04 20:58:03 -0300

    adicionado botões para rotas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,12 @@
 function Login(){
-    return <h1>Login</h1>
+    return (
+        <>
+            <h1>Login</h1>
+            <a href="/home">home</a>
+            <a href="/cadastro">cadastro</a>
+        </>
+    )
+
 }
 
 export default Login;
