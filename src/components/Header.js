cee5b19db3ec569d4c760f92a027f580e8c23f80
Rafael <rafael.abreu@allycode.com.br>
2021-11-06 02:10:59 -0300

    adicionado e estilizado o header

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+import logo from "../img/logo.png"
+import perfil from "../img/perfil-user.png"
+
+import styles from '../styles/components/Header.module.css';
+
+function Header(){
+    return (
+        <>
+            <div className={styles.header}>
+                <Link to="/home">
+                    <img src={logo} alt="logo"/>
+                </Link>
+                <div className={styles.perfil}>
+                    <div className={styles.arrow}></div>
+                    <img src={perfil} alt="perfil"/>
+                <div className={styles.sidebar}>
+                <Link to="/cadastro">
+                    Logout
+                </Link>
+                </div>
+
+                </div>
+            </div>
+        </>
+
+
+    )
+    
+};
+
+export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
+
+import styles from '../styles/pages/Home.module.css';
+
+import Header from '../components/Header'
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <>
+            <Header/>
+            <div className={styles.bgHome}>
+            <h1>Home</h1>
+
+            </div>
+        </>
+    )
 }
 
 export default Home;
