a754704bfadc37df01a3cf16a2eb04694862bed9
Rafael <rafael.abreu@allycode.com.br>
2021-11-10 22:09:20 -0300

    adicionando loop de vídeos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,42 +1,14 @@
-import {React, useState, useEffect} from 'react';
-import axios from 'axios';
 
 import styles from '../styles/components/Video.module.css';
 
 
-const apiUrl = 'https://tubflix-api.herokuapp.com/api/v1';
 
 function Videos(){
 
-    const [videos, setVideos] = useState([]);
-    // const [authToken, setAuthToken] = useState(false);
-
-
-    useEffect(() => {
-        axios.get(`${apiUrl}/videos`, 
-        {   
-            headers: {
-                'content-type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem("api-token")}`
-            }
-        })
-        .then(function (response) {
-            setVideos(response.data)
-            // console.log(`${apiUrl}/videos`)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    }, []);
-
-            console.log(videos);
 
     return (
         <>
-            <div className={styles.heroContent}>
-
-            </div>
+         <h1>components video</h1>
         </>
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,53 @@
 
 import styles from '../styles/pages/Home.module.css';
 import { Navigate } from "react-router-dom";
+import {React, useState, useEffect} from 'react';
+import axios from 'axios';
 
 import Header from '../components/Header'
 import Videos from '../components/Videos'
 
+const apiUrl = 'https://tubflix-api.herokuapp.com/api/v1';
+
 function Home(props){
+
+    const [videos, setVideos] = useState([]);
+    // const [authToken, setAuthToken] = useState(false);
+
+
+    useEffect(() => {
+        axios.get(`${apiUrl}/categories`, 
+        {   
+            headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem("api-token")}`
+            }
+        })
+        .then(function (response) {
+            setVideos(response.data)
+            // console.log(`${apiUrl}/videos`)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, []);
+
+            console.log(videos);
 
     return (
         <>
             {props.authToken ? '' : <Navigate to="/" /> }
             <Header/>
             <div className={styles.bgHome}>
-            <Videos />
+                <section>
+                    {videos.map((item, key)=>{
+                        <div>
+                            {item.data.name}
+                        </div>
+                    })}
+                </section>
+                <Videos/>
             </div>
         </>
     )
