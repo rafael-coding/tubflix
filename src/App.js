77bb0045720e1bcdb018a0df825beb69e7d2877b
Joao P. F. Ferreira <jp.contatoprofissional@gmail.com>
2021-11-16 13:07:15 -0300

    page 404

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import {React, useState, useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { React, useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Logout from "./pages/Logout";
-import './styles/Global.css';
-
-
+import Page404 from "./pages/Page404";
+import "./styles/Global.css";
 
 function App() {
   // const navigate = useNavigate();
-  
+
   const [authToken, setAuthToken] = useState(false);
 
   // const authRoutes = () => {
@@ -19,7 +18,6 @@ function App() {
   //   }
   // }
 
-
   //get token at localstorage
   useEffect(() => {
     setAuthToken(localStorage.getItem("api-token"));
@@ -27,14 +25,21 @@ function App() {
   }, [authToken]);
 
   return (
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Login authToken={authToken} setAuthToken={setAuthToken}/>} />
-            <Route path="/cadastro" element={<Register />} /> 
-            <Route path="/logout" element={<Logout authToken={authToken} setAuthToken={setAuthToken} />} /> 
-            <Route path="/home" element={<Home authToken={authToken} />}/> 
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Login authToken={authToken} setAuthToken={setAuthToken} />}
+        />
+        <Route path="/cadastro" element={<Register />} />
+        <Route
+          path="/logout"
+          element={<Logout authToken={authToken} setAuthToken={setAuthToken} />}
+        />
+        <Route path="/home" element={<Home authToken={authToken} />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Page404.js
@@ -0,0 +1,15 @@
+function Page404() {
+  return (
+    <h1
+      style={{
+        color: "white",
+        textAlign: "center",
+        marginTop: "50vh",
+      }}
+    >
+      Tá perdido mano, isso claramente é um 404.
+    </h1>
+  );
+}
+
+export default Page404;
