ea2cff3f7e0fc23b3881b725b29721b1aaa97ce1
m_ronald <michael.ronald@allycode.com.br>
2021-12-06 10:21:08 -0300

    islogged in context

--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -7,7 +7,7 @@ const LoginProvider = ({ children }) => {
     const [token, setToken] = useState('');
     const [validEmail, setValidEmail] = useState('');
     const [validPassword, setValidPassword] = useState('');
-
+    const [message, setMessage] = useState('');
 
     function login(email, password) {
         email = 'admin@admin.com.br';
@@ -24,6 +24,7 @@ const LoginProvider = ({ children }) => {
         }).then(
             response => {
                 if ('error' in response.data) {
+                    setMessage(response.error);
                     alert('Erro genérico login');
                 } else if ('access_token' in response.data) {
                     // Salvando email e senha para uso posterior
@@ -31,6 +32,9 @@ const LoginProvider = ({ children }) => {
                     setValidPassword(password);
                     // Salvando token para acesso global
                     setToken(response.data.access_token);
+
+                    // Resetando mensagem
+                    setMessage('');
                 }
             }
         ).catch(
@@ -45,12 +49,42 @@ const LoginProvider = ({ children }) => {
         login(validEmail, validPassword);
     }
 
+    function logout() {
+        axios.post(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
+            headers: {
+                "content-type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            }
+        }).then(
+            response => {
+                if ('message' in response.data && response.data.message === 'Successfully logged out') {
+                    setToken('');
+                    setMessage(response.data.message);
+                    alert('logout bem sucedido');
+                } else {
+                    alert('deu merda no logout');
+                }
+            }
+        ).catch(
+            error => {
+                alert('deu merda no logout');
+            }
+        );
+    }
+
+    function isLogged() {
+        return token !== '' && message === '';
+    }
+
     return (
         <LoginContext.Provider
             value={{
                 token,
                 login,
+                logout,
                 loginExpired,
+                isLogged,
             }}
         >
             {children}
